58ad9747f7b671c2126be19754ec79ed2b46e978
Elias-Isaac Phiri <phirielias@gmail.com>
2018-01-28 09:44:32 +0200

    Satellites and Orbits

---
+++ Assets/Orbit1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit1 : MonoBehaviour
+{
+
+    public GameObject Satellite1; // the cube game obj that will orbit around
+    public float speed; // speed of the orbiting red cube 
+                        // use this for initialization
+
+    void Start(){
+        
+    }
+
+    //update is called once per frame
+    void Update(){
+        OrbitAround(); // This function or method will make the red cube orbit around the other
+    }
+
+    void OrbitAround(){
+        transform.RotateAround(Satellite1.transform.position, Vector3.up, speed * Time.deltaTime); // make red cube orbit around the white cube
+    }
+
+
+
+
+
+}
